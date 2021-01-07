a506048a6e150e42a455319b4aed5f17d1042ea4
Zuzze <nevalsus@gmail.com>
2021-01-07 17:00:46 +0100

    new project initialized

---
+++ hashtags.ts
@@ -0,0 +1,62 @@
+export const HASHTAGS = {
+  app: [],
+  web: [],
+  logo: [],
+  graphics: [],
+  "3d": [],
+  design: ["#designbymonday", "#designbymonday", "uiuxsupply"],
+  flat: ["#flatdesigndaily", "#flatdesigninspiration"]
+};
+
+export const USERS = {
+  design: [
+    "@uisquirrel",
+    "@ui_ux_idea",
+    "@uisociety",
+    "@designbymonday",
+    "@uiuxbunker",
+    "@uxbrainy",
+    "@designquokka",
+    "@uxlean",
+    "@appliedcreatives",
+    "@uxdesigner.world",
+    "@ui.interceptor",
+    "@delightful.ui",
+    "@ux_trends",
+    "@dribbblepro",
+    "@ui.designs",
+    "@ronasit_ui",
+    "@uidailyworks",
+    "@uiuxlabz",
+    "@uxllusion",
+    "@ui.squirrel",
+    "@purrweb_agency",
+    "@ui.markin",
+    "@dailyuix",
+    "@uiuxmobile",
+    "@uxbucket",
+    "@uilabz",
+    "@ui.dose",
+    "@uiuxdailydesign",
+    "@uiuxsupply",
+    "@uiuxbkr"
+  ],
+  logo: ["@logobucket", "@icons8", "@logohexa"],
+  app: [
+    "@juicyappdesign",
+    "@dopelydesign",
+    "@uixglow",
+    "@uisource",
+    "@ui.design",
+    "@_designbakery",
+    "@ui.interceptor"
+  ],
+  web: ["@websitebrainy", "@webkraftr_", "@webdeveloperibs", "@webdesign.clan"],
+  flat: [
+    "@_flatdesign",
+    "@flatdesigndaily",
+    "@flatdesignspiration",
+    "@flatdesign.trend"
+  ],
+  "3d": []
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./hashtag.png";
+import "./App.css";
 
-function App() {
+const NUMBER_OF_HASHTAGS = 30;
+
+const FILTERS: Array<string> = [
+  "design",
+  "dev",
+  "logo",
+  "mobile",
+  "tablet",
+  "graphics",
+  "flat",
+  "3d"
+];
+
+const App: React.FC = () => {
+  const [selectedFilters, setSelectedFilters] = useState<Array<string>>([]);
+  const [text, setText] = useState<string>("");
+  const [numberOfTaggedUsers, setNumberOfTaggedUsers] = useState<number>(10);
+
+  function generateHashtags(): void {
+    let newText: string = "";
+    setText(newText);
+  }
+
+  function updateFilters(filter: string): void {
+    const updatedFilters: Array<string> = selectedFilters;
+    if (selectedFilters.indexOf(filter) > -1) {
+      updatedFilters.filter(f => f !== filter);
+    } else {
+      updatedFilters.push(filter);
+    }
+    setSelectedFilters(updatedFilters);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          {FILTERS.map(filter => (
+            <span key={filter} style={{ marginRight: "20px" }}>
+              <input
+                id={filter}
+                name={filter}
+                type="checkbox"
+                onChange={() => updateFilters(filter)}
+              />
+              <label>{filter}</label>
+            </span>
+          ))}
+        </div>
+        <br />
+        <div>
+          <label>Number of tagged users</label>
+          <br />
+          <input
+            name="users"
+            type="number"
+            value={numberOfTaggedUsers}
+            onChange={e => setNumberOfTaggedUsers(Number(e.target.value))}
+          />
+        </div>
+        <br />
+        <button onClick={generateHashtags}>Generate hashtags</button>
+        <textarea value={text} />
       </header>
+      <main></main>
     </div>
   );
-}
+};
 
 export default App;
